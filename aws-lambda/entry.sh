8174159225624cd80dd8978e40081a5388fd0a5e
Ron <tahmeat@meatsmotel.com>
2024-06-26 18:32:48 -0500

    Delete aws-lambda directory
    back-end

--- aws-lambda/entry.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# for local testing, running an AWS Lambda Runtime Interface Emulator.
-
-if [ -z "${AWS_LAMBDA_RUNTIME_API}"]; then
-    exec /usr/bin/aws-lambda-rie /usr/local/bin/python -m awslambdaric "$@"
-else
-    exec /usr/local/bin/python -m awslambdaric "$@"
-fi
